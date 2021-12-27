298d5957a718492eb7b74e6f4b3a739bdd47b25d
Husban Ahmed <husban13@gmail.com>
2021-12-27 15:29:34 -0800

    Initial Commit

---
+++ MyFridge2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFridge2.0
+//
+//  Created by Husban Ahmed on 12/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
